812f783680f2c2331f5b7b0ff78921643a9bd006
Zach Hildreth <41015215+zachhildreth@users.noreply.github.com>
2022-05-23 17:27:31 -0700

    Change url structure
    Change url structure

--- turbotx-backend.go
+++ turbotx-backend.go
@@ -64,7 +64,7 @@ type ErrorResults struct {
 }
  
 // global constants
-const URL string = "https://xcash.foundation/"
+const URL string = "http://162.55.235.87?id="
 const letterBytes = "0123456789"
 const IDLENGTH = 5
 const BLOCK_VERIFIER_VALID_AMOUNT = 9
