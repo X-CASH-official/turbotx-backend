1c042ddfdb16b84ca86847f73900d0c1bcc6578e
Zach Hildreth <41015215+zachhildreth@users.noreply.github.com>
2022-07-06 15:44:32 -0700

    Update turbotx-backend.go

--- turbotx-backend.go
+++ turbotx-backend.go
@@ -64,7 +64,7 @@ type ErrorResults struct {
 }
  
 // global constants
-const URL = "http://162.55.235.87/?id="
+const URL = "http://turbotx.xcash.foundation/?id="
 const letterBytes = "0123456789"
 const IDLENGTH = 5
 const BLOCK_VERIFIER_TOTAL_AMOUNT = 14
